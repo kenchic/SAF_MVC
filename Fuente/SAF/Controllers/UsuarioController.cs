380a5df25b95d6951b1ca830ab9b0d6814143b8d
Ing. German Alvarez <ayzcorp@hotmail.com>
2018-09-14 20:50:35 -0500

    REQ

--- Fuente/SAF/Controllers/UsuarioController.cs
+++ Fuente/SAF/Controllers/UsuarioController.cs
@@ -19,27 +19,22 @@ namespace SAF.Controllers
 		public ActionResult UsuarioMenu()
 		{
 			try
-			{
-				//string strMenu = "<section class=\"animate-menu animate-menu-left\">";
-				//strMenu += "<ul class=\"sidebar-menu-rtl\">";
-				//strMenu += "<li class=\"sidebar-header\">OPCIONES</li>";
-				//strMenu += "<li>";
-				//strMenu += "<a href=\"#\" >";
-				//strMenu += "<i class=\"fa fa-files-o\"></i>";
-				//strMenu += "<span>Agente</span>";
-				//strMenu += "<span class=\"label label-primary pull-right\">4</span>";
-				//strMenu += "</a>";
-				//strMenu += "<ul class=\"sidebar-submenu\" style=\"display: none;\">";
-				//strMenu += "<li><a href=\"#\" onclick=\"cargarView('/Agente/Listar');\"><i class=\"fa fa-circle-o\"></i>Listar</a></li>";
-				//strMenu += "<li><a href=\"#\" onclick=\"cargarView('/Agente/inicio');\"><i class=\"fa fa-circle-o\"></i>Inicio 2</a></li>";
-				//strMenu += "<li class=\"\">";
-				//strMenu += "<a href=\"collapsed-sidebar.html\"><i class=\"fa fa-circle-o\"></i> Collapsed Sidebar</a>";
-				//strMenu += "</li>";
-				//strMenu += "</ul>";
-				//strMenu += "</li>";
-				//strMenu += "</ul>";
-				//strMenu += "</section>";
-				//return strMenu;
+			{				
+				string strMenu = "<li>";
+				strMenu += "<a href=\"#\" >";
+				strMenu += "<i class=\"fa fa-files-o\"></i>";
+				strMenu += "<span>Agente</span>";
+				strMenu += "<span class=\"label label-primary pull-right\">4</span>";
+				strMenu += "</a>";
+				strMenu += "<ul class=\"sidebar-submenu\" style=\"display: none;\">";
+				strMenu += "<li><a href=\"#\" onclick=\"cargarView('/Agente/Listar');\"><i class=\"fa fa-circle-o\"></i>Listar</a></li>";
+				strMenu += "<li><a href=\"#\" onclick=\"cargarView('/Agente/inicio');\"><i class=\"fa fa-circle-o\"></i>Inicio 2</a></li>";
+				strMenu += "<li class=\"\">";
+				strMenu += "<a href=\"collapsed-sidebar.html\"><i class=\"fa fa-circle-o\"></i> Collapsed Sidebar</a>";
+				strMenu += "</li>";
+				strMenu += "</ul>";
+				strMenu += "</li><script>$.sidebarMenu($('.sidebar-menu'));</script>";
+				ViewBag.Tree = strMenu;
 				return View();
 			}
 			catch (Exception ex)
