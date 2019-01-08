b9a0f7c0b13fdffe463cf40f4d5eb0139f4f75d9
Ing. German Alvarez <ayzcorp@hotmail.com>
2019-01-08 14:38:48 -0500

    Creaciòn de archivos

---
+++ REQ/REQ_9/Scripts/REQ_9_2_DDL_SAF_GAV.sql
@@ -0,0 +1,88 @@
+USE [SAF]
+GO
+
+/****** Object:  StoredProcedure [dbo].[pListaPrecio]    Script Date: 03/01/2019 09:04:41 p.m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[pListaPrecio]  
+(
+	@Accion INT = 0, --0:Listar Todos, 1: Listar Activos, 2: Consultar, 3: Insertar, 4: Editar, 5: Borrar
+	@Json NVARCHAR(max)	
+)
+AS 
+BEGIN
+	IF (@Accion = 0)
+		SELECT Id, Nombre, Activo FROM bdListaPrecio
+
+	IF (@Accion = 1)
+		SELECT Id, Nombre, Activo FROM bdListaPrecio WHERE Activo = 1
+
+	IF(@Accion = 2)
+		BEGIN
+			SELECT Id, Nombre, Activo FROM bdListaPrecio
+			WHERE Id = 
+			(SELECT      
+				   max(CASE WHEN name='Id' THEN convert(int,StringValue) ELSE 0 END) AS [Id]
+			FROM SAFseg.dbo.fParseJSON
+			(
+				  @json
+			)) 
+		END
+
+	IF(@Accion = 3)
+		BEGIN
+			INSERT INTO bdListaPrecio 
+			SELECT * FROM (SELECT
+					max(CASE WHEN name='Nombre' THEN convert(VARCHAR(100),StringValue) ELSE '' END) AS [Nombre],
+					max(CASE WHEN name='Activo' THEN convert(BIT,StringValue) ELSE 0 END) AS [Activo]
+			FROM SAFseg.dbo.fParseJSON
+			( @Json )
+			WHERE ValueType = 'string' OR ValueType = 'boolean'
+			GROUP BY parent_ID) ListaPrecio
+		END
+
+	IF(@Accion = 4)
+		BEGIN
+
+			UPDATE A
+			SET Nombre = ListaPrecio.Nombre,
+				Activo = ListaPrecio.Activo
+			FROM bdListaPrecio AS A
+			INNER JOIN 
+			(SELECT
+				   max(CASE WHEN name='Nombre' THEN convert(VARCHAR(100),StringValue) ELSE '' END) AS [Nombre],
+				   max(CASE WHEN name='Activo' THEN convert(bit,StringValue) ELSE 0 END) AS [Activo],
+				   max(CASE WHEN name='Id' THEN convert(int,StringValue) ELSE 0 END) AS [Id]
+			FROM SAFseg.dbo.fParseJSON
+			(
+				  @json
+			)) AS ListaPrecio ON A.Id = ListaPrecio.Id
+
+		END
+
+	IF(@Accion = 5)
+		BEGIN
+			DELETE A
+			FROM bdListaPrecio AS A
+			INNER JOIN 
+			(SELECT      
+				   max(CASE WHEN name='Id' THEN convert(INT,StringValue) ELSE 0 END) AS [Id]
+			From SAFseg.dbo.fParseJSON
+			(
+				  @json
+			)) AS ListaPrecio ON A.Id = ListaPrecio.Id
+		END
+END
+
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'Version', @value=N'19.0.1' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'PROCEDURE',@level1name=N'pListaPrecio'
+GO
+
+
+
