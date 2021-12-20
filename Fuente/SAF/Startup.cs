b6e10a8bca4da403c334aa42ffb113fb8d9cd1d9
German Enrique Álvarez Velandia <german.alvarez@actsis.com>
2021-12-20 09:22:59 -0500

    Creación depantalla Remisiones

--- Fuente/SAF/Startup.cs
+++ Fuente/SAF/Startup.cs
@@ -33,8 +33,7 @@ namespace SAF
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-			
+        {			
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
